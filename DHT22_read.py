73f1c5e7da316dc4290aa58dcf1af8ad8364d220
Pilar Zhang Qiu <32870045+pz716@users.noreply.github.com>
2019-12-29 18:19:41 +0100

    Update DHT22_read.py

--- DHT22_read.py
+++ DHT22_read.py
@@ -1,11 +1,18 @@
 import Adafruit_DHT
 
-def DHTread(sensor,pin)
+def DHTread(pin)
+
+  # Sensor should be set to Adafruit_DHT.DHT11,
+  # Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+  sensor = Adafruit_DHT.DHT22
+  
   # Try to grab a sensor reading.  Use the read_retry method which will retry up
   # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-  humidity, temperature = Adafruit_DHT.read_retry(sensorDHT, pin)
+  humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
   
   if humidity is not None and temperature is not None:
     continue
   else:
     print('Failed to get reading. Try again!')
+    
+  return humidity, temperature;
