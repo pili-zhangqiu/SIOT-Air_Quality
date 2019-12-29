a98bf39027cc179b4f8fddd1a2a0b8edb44a8ed7
Pilar Zhang Qiu <32870045+pz716@users.noreply.github.com>
2019-12-29 19:34:18 +0100

    Update G_Log.py

--- G_Log.py
+++ G_Log.py
@@ -3,6 +3,9 @@ from __future__ import print_function
 import datetime
 import time
 
+# Import the function to update Google Sheet
+from G_UpdateSheet import update_sheet
+
 # Import the function reading the ADC values from both MQ135 sensors (Air Quality)
 from ADS1x15_read import ADCread
 
@@ -15,7 +18,6 @@ def main():
         
         # Read air quality values from the MQ135 sensors
         ADC_values = ADCread()
-        print (ADC_values)
 
         ADC0 = ADC_values[0]    # Air Quality Inside
         ADC1 = ADC_values[1]    # Air Quality Outside
@@ -24,13 +26,17 @@ def main():
         DHT_pin0 = 23
         DHT_pin1 = 24
 
+        DHT_t0, DHT_h0 = DHTread(DHT_pin0)
+        DHT_t1, DHT_h1 = DHTread(DHT_pin1)
+
+
         # Print values on the command prompt
         print ('Air Quality: (Inside) %f PPM' % ADC0)
         print ('Air Quality: (Outside) %f PPM' % ADC1)
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))       
         print ('-' *30)
 
-        update_sheet("Sheet1", ADC0, ADC1)
+        update_sheet("Sheet1", ADC0, DHT_t0, DHT_h0, ADC1, DHT_t1, DHT_h1)
         time.sleep(60)
 
 if __name__ == '__main__':  
