8a9f974532c7530868de7d2e5f7a313b31024905
Pilar Zhang Qiu <32870045+pz716@users.noreply.github.com>
2019-12-31 03:45:46 +0100

    Update G_UpdateSheet.py

--- G_UpdateSheet.py
+++ G_UpdateSheet.py
@@ -1,7 +1,7 @@
 # Import Google Sheets API libraries
 from __future__ import print_function  
 from googleapiclient.discovery import build  
-# from httplib2 import Http  
+from httplib2 import Http  
 # from oauth2client import file, client, tools  
 from oauth2client.service_account import ServiceAccountCredentials  
 import datetime
