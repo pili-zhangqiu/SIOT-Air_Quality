78ba57b0382ec21b175ab8d2b61c3634c4fa41be
Pilar Zhang Qiu <32870045+pz716@users.noreply.github.com>
2019-12-31 02:55:31 +0100

    Update G_Log.py

--- G_Log.py
+++ G_Log.py
@@ -13,7 +13,6 @@ from ADS1x15_read import ADCread
 from DHT22_read import DHTread
 
 #Others
-from __future__ import print_function  
 from googleapiclient.discovery import build  
 from httplib2 import Http  
 from oauth2client import file, client, tools  
