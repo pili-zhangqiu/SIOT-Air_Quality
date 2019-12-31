476210055e8a50ad58e19c57269c9c1fb3cd6439
Pilar Zhang Qiu <32870045+pz716@users.noreply.github.com>
2019-12-31 04:17:38 +0100

    Update G_UpdateSheet.py

--- G_UpdateSheet.py
+++ G_UpdateSheet.py
@@ -14,7 +14,7 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-def update_sheet(sheetname, AirQy0, AirQy1, temp0, hum0, temp1, hum1):  
+def update_sheet(sheetname, AirQy0, PPM0, AirQy1, PPM1, temp0, hum0, temp1, hum1):  
     """update_sheet method:
        appends a row of a sheet in the spreadsheet with the 
        the latest temperature, pressure and humidity sensor data
@@ -57,7 +57,7 @@ def update_sheet(sheetname, AirQy0, AirQy1, temp0, hum0, temp1, hum1):
     # Call the Sheets API, append the next row of sensor data
     # values is the array of rows we are updating, its a single row
     values = [ [ str(datetime.datetime.now()), 
-        AirQy0 , temp0, hum0, AirQy1, temp1, hum1 ] ]
+        AirQy0 , PPM0, temp0, hum0, AirQy1, PPM1, temp1, hum1 ] ]
     body = { 'values': values }
     
     # call the append API to perform the operation
