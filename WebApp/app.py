ce11cdc14e28f4dcc28ff308f5a2be40cd2a470d
Pilar Zhang Qiu <32870045+pz716@users.noreply.github.com>
2020-01-10 19:49:11 +0000

    Update app.py

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Define the main route:
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
+# Other routes are:
+@app.route('/cakes')
+def cakes():
+    return 'Yummy cakes!'
 
 if __name__ == '__main__':
+    # Run code server and app:
     app.run(debug=True, host='0.0.0.0')
