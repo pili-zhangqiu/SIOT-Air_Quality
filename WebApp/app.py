35136ec56efdaf5cebd465236dd1942b9d7c250d
Pilar Zhang Qiu <32870045+pz716@users.noreply.github.com>
2020-01-11 03:12:47 +0000

    Update app.py

--- WebApp/app.py
+++ WebApp/app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# Other routes are:
-@app.route('/cakes')
-def cakes():
-    return 'Yummy cakes!'
+@app.route("/data_report")
+def chart():
+    legend = 'Monthly Data'
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    return render_template('chart.html', values=values, labels=labels, legend=legend)
 
 if __name__ == '__main__':
     # Run code server and app:
