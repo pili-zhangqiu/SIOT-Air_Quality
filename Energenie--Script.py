69265777db28a65a546c70d0aca8dbab3869e2b7
Pilar Zhang Qiu <32870045+pz716@users.noreply.github.com>
2020-01-12 20:29:22 +0000

    Create Energenie--Script.py

---
+++ Energenie--Script.py
@@ -0,0 +1,44 @@
+import time
+import RPi.GPIO as GPIO
+ 
+# Set the GPIO numbering scheme
+GPIO.setmode(GPIO.BCM)
+ 
+# Select the GPIO pins used for
+# the encoder D0-D3 data inputs
+GPIO.setup(17,GPIO.OUT,initial=0)
+GPIO.setup(22,GPIO.OUT,initial=0)
+GPIO.setup(23,GPIO.OUT,initial=0)
+GPIO.setup(27,GPIO.OUT,initial=0)
+ 
+# Select the GPIO pin to enable/disable the modulator
+# Default to disabled
+GPIO.setup(25, GPIO.OUT,initial=0)
+ 
+# Select the signal used to select ASK/FSK
+# Default to ASK
+GPIO.setup(24, GPIO.OUT,initial=0)
+
+# Socket 1 ON
+D3=True
+D2=True
+D1=True
+D0=True
+ 
+# Set D0-D3
+GPIO.output (27, D3)
+GPIO.output (23, D2)
+GPIO.output (22, D1)
+GPIO.output (17, D0)
+ 
+# Let it settle, encoder requires this
+time.sleep(0.1)
+ 
+# Enable the modulator
+GPIO.output (25, True)
+ 
+# Keep enabled for a period
+time.sleep(0.25)
+ 
+# Disable the modulator
+GPIO.output (25, False)
